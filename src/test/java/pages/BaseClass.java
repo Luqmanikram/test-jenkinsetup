8b5866a5924c20dfb77489c2d6fb4ceef0177755
Luqmanikram <Ikramluqman36@gmail.com>
2023-11-13 20:56:37 -0600

    Add chrome optios for headless driver

--- src/test/java/pages/BaseClass.java
+++ src/test/java/pages/BaseClass.java
@@ -2,18 +2,22 @@ package pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseClass {
     public static WebDriver driver;
+    public static ChromeOptions options;
     public void open_chrome_browser(){
         String url="https://abc9262.sg-host.com/";
         System.setProperty(
                 "webdriver.chrome.driver",
                 "C:\\wordspace\\Selenium-jenkins\\chromedriver_119.exe");
+         options =new ChromeOptions();
+         options.addArguments("--headless");
         // Instantiate a ChromeDriver class.driver = new ChromeDriver();
-        driver=new ChromeDriver();
+        driver=new ChromeDriver(options);
         driver.get(url);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS );
--- src/test/resources/Features/registration.feature
+++ src/test/resources/Features/registration.feature
@@ -1,8 +1,8 @@
 Feature: Verify Registration Functionality
   Scenario: New customer registration
     Given I am on the registration page
-    When I enter a valid Username "Jon5333fffavv13D66oe123"
-    And I enter a valid Email address "jo5hnffaaa163336oe@example.com"
-    And I enter a valid Password "joho567eaffaaa1333@example.com"
+    When I enter a valid Username "Jon566f123"
+    And I enter a valid Email address "jo5haoe@example.com"
+    And I enter a valid Password "joho333@example.com"
     And I submit the registration form
     Then I should be automatically redirected to the customer login page
